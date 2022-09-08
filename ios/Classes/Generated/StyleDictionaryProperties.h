4576f6739e214167489f4666b7dee7b5c73139a6
Alex Finnarn <alex.finnarn@gmail.com>
2022-09-08 12:55:07 -0400

    adding complete example files

---
+++ ios/Classes/Generated/StyleDictionaryProperties.h
@@ -0,0 +1,19 @@
+
+//
+// StyleDictionaryProperties.h
+//
+
+// Do not edit directly
+// Generated on Thu, 08 Sep 2022 16:53:34 GMT
+
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface StyleDictionaryProperties : NSObject
+
++ (NSDictionary *)properties;
++ (NSDictionary *)getProperty:(NSString *)keyPath;
++ (nonnull)getValue:(NSString *)keyPath;
+
+@end
